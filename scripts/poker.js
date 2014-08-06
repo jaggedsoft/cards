d2d152826a697e9c72eb807ad9d45877625bfef7
Derek Ries <influenztial@gmail.com>
2014-08-05 19:33:23 -0700

    testing

--- scripts/poker.js
+++ scripts/poker.js
@@ -1,3 +1,15 @@
 require(['card', 'deck'], function (Card, Deck) {
   // Fuck you
+
+
+  function winner (a, b) {
+    return a;
+  }
+
+  function m (str) {
+
+  }
+
+  winner(m('5H 5C 6S 7S KD'), m('2C 3S 8S 8D TD'));
+
 });
