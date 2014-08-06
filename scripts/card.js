8eb5b140498464a68c8920eebed78d96937c5b0f
Derek Ries <influenztial@gmail.com>
2014-08-05 17:07:24 -0700

    more cards

--- scripts/card.js
+++ scripts/card.js
@@ -1,7 +1,20 @@
 define([], function () {
 
-  var Card = function () {
+  var CARDS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
 
+  var Card = function (suit, number) {
+    this.suit = suit;
+    this.number = number;
+    this.value = CARDS[number-1];
+  };
+
+  Card.prototype.toString = function() {
+    // 'Jack of Clubs', '5 of Hearts'
+
+  };
+
+  Card.prototype.compare = function () {
+    // body...
   };
 
   return Card;
