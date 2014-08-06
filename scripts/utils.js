314b38bd65a3bac820a42017f7d538a65a93551b
Nick Ruiz <nick@fatcatonline.net>
2014-08-05 17:19:21 -0700

    deck, utils

---
+++ scripts/utils.js
@@ -0,0 +1,11 @@
+define([], function () {
+
+    var utils = {
+        capitalize: function (string) {
+            return string.substr(0,1).toUpperCase() + string.substr(1);
+        }
+    }
+
+    return utils;
+
+});
