c15e67cd915b6190420a72e06e9dcade023156f3
Nick Ruiz <nick@fatcatonline.net>
2014-08-05 21:30:16 -0700

    poker methods

--- scripts/poker.js
+++ scripts/poker.js
@@ -1,15 +1,66 @@
-require(['card', 'deck'], function (Card, Deck) {
+require(['card', 'deck', 'utils'], function (Card, Deck, Utils) {
   // Fuck you
 
+  function sameSuit(hand) {
+    return _.every(hand, {'suit': hand[0].suit});
+  }
+
+  function isStraight(hand) {
+    return Utils.consecutive(_.sortBy(_.map(hand, 'number')));
+  }
 
   function winner (a, b) {
     return a;
   }
 
-  function m (str) {
+  function mc (str) {
+    var number = str.substr(0,1);
+    var suit = str.substr(1);
 
+    var numberMap = {
+      'T' : 10,
+      'J' : 11,
+      'Q' : 12,
+      'K' : 13,
+      'A' : 14
+    }
+
+    var suitMap = {
+      'C' : 'clubs',
+      'H' : 'hearts',
+      'S' : 'spades',
+      'D' : 'diamonds'
+    }
+
+    if(numberMap.hasOwnProperty(number)){
+      number = numberMap[number];
+    } else {
+      number = parseInt(number);
+    }
+
+    return new Card(suitMap[suit], number);
   }
 
-  winner(m('5H 5C 6S 7S KD'), m('2C 3S 8S 8D TD'));
+  function m (str) {
+    return _.map(str.split(" "), mc);
+  }
 
+  // var testCard = mc('5H');
+  // console.log(testCard.number === 5);
+  // console.log(testCard.suit === 'hearts');
+  // console.log(typeof testCard.number);
+
+  // var testCard = mc('TH');
+  // console.log(testCard.number === 10);
+
+  console.log(sameSuit(m('5H 5H 6H 7H KH')));
+  console.log(sameSuit(m('5H 5H 6H 7H KS')));
+
+  console.log(Utils.consecutive([1,2,3,4,5]));
+  console.log(Utils.consecutive([2,1,3,4,5]));
+
+  console.log(isStraight(m('5H 6H 7H 8H 9H')));
+  console.log(isStraight(m('9H 8H 6H 7H 5H')));
+  console.log(isStraight(m('5H 3H 6H 7H KH')));
+  console.log(isStraight(m('5H 5D 6H 7H 8H')));
 });
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,15 @@
 define([], function () {
 
     var utils = {
+
         capitalize: function (string) {
-            return string.substr(0,1).toUpperCase() + string.substr(1);
+          return string.substr(0,1).toUpperCase() + string.substr(1);
+        },
+
+        consecutive: function (numbers) {
+          return _.isEqual(numbers, _.range(numbers[0], _.last(numbers) + 1));
         }
-    }
+    };
 
     return utils;
 
